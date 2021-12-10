4ad34c8e196d3152408da474fd7b259a906dee2a
yuehengshi <shiyueheng@gmail.com>
2021-12-10 10:58:32 -0500

    test

--- Podfile
+++ Podfile
@@ -5,7 +5,7 @@ inhibit_all_warnings!
 target 'GetStreamActivityFeed' do
   use_frameworks!
   pod 'GetStream'
-  pod 'Nuke', '~> 8.1'
+  pod 'Nuke', '~> 10.0'
   pod 'Reusable', '~> 4.1'
   pod 'SnapKit', '~> 5.0'
 end
--- Sources/Components/Image Gallery/ImageGalleryViewController.swift
+++ Sources/Components/Image Gallery/ImageGalleryViewController.swift
@@ -178,7 +178,7 @@ public final class ImageGalleryCollectionViewCell: UICollectionViewCell, Reusabl
                 self.imageView.image = .imageIcon
             }
             
-            completion(result.error)
+            //completion(result.error)
         }
     }
 }
